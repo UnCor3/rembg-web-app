44b2204ef4d8e5e10be086bed9989d6794dc19db
Uncor3 <contactuncor3@gmail.com>
2023-12-07 08:21:18 +0300

    update api url for codesandbox

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,13 +1,13 @@
 import { ImagesInitialType } from "../components/rembg-form/rembg-form.component";
 
 //Backend is served on a different port
-const API_PORT = 3001;
+const API_PORT = "3001";
 const API_PROTOCOL = document.location.protocol;
 const API_HOSTNAME = document.location.hostname;
 const VITE_PORT = import.meta.env.VITE_PORT;
 
 //*API URL
-export const API_URL = `${API_PROTOCOL}//${API_HOSTNAME}:${API_PORT}`;
+export const API_URL = document.location.href.replace("3005", API_PORT);
 export const VITE_PUBLIC_URL = `${API_PROTOCOL}//${API_HOSTNAME}:${VITE_PORT}`;
 
 export const GITHUB_PROFILE = "https://github.com/UnCor3";
